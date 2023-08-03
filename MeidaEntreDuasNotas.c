be5897e03c5d1642327afd9cf4954b79e3b7a115
glVerdi <gabrielVerdi12@gmail.com>
2023-08-02 23:46:51 -0300

    change note to grade

--- MeidaEntreDuasNotas.c
+++ MeidaEntreDuasNotas.c
@@ -3,13 +3,13 @@
 
 void main(){
 
-    //Average between two notes
-    float nota1/*note1*/, nota2/*note2*/, media/*averege*/;
-    printf("Digite a primeira nota: ");//Enter the first note
-    scanf("%f", &nota1/*note1*/);
-    printf("Digite a segunda nota: ");//Enter the second note
-    scanf("%f", &nota2/*note2*/);
-    media/*average*/ = (nota1/*note1*/ + nota2/*note2*/) / 2;
+    //Average between two grades
+    float nota1/*grade1*/, nota2/*grade2*/, media/*averege*/;
+    printf("Digite a primeira nota: ");//Enter the first grade
+    scanf("%f", &nota1/*grade1*/);
+    printf("Digite a segunda nota: ");//Enter the second grade
+    scanf("%f", &nota2/*grade2*/);
+    media/*average*/ = (nota1/*grade1*/ + nota2/*grade2*/) / 2;
     printf("A media entre a %f e %f eh: %f", nota1, nota2, media);//The average between %f and %f is
 
 }
--- ValorAbsolutoEntreDuasNotas.c
+++ ValorAbsolutoEntreDuasNotas.c
@@ -3,13 +3,13 @@
 
 void main(){
 
-    //Absolute Value Between Two Notes
-    int nota1/*note1*/, nota2/*note2*/, diferenca/*difference*/;
-    printf("Digite a primeira nota: ");//Enter the first no
-    scanf("%d", &nota1/*note1*/);
-    printf("Digite a segunda nota: ");//Enter the second no
-    scanf("%d", &nota2/*note2*/);
-    diferenca/*difference*/ = nota1/*note1*/ - nota2/*note2*/;
+    //Absolute Value Between Two Grades
+    int nota1/*grade1*/, nota2/*grade2*/, diferenca/*difference*/;
+    printf("Digite a primeira nota: ");//Enter the first grade
+    scanf("%d", &nota1/*grade1*/);
+    printf("Digite a segunda nota: ");//Enter the second grade
+    scanf("%d", &nota2/*grade2*/);
+    diferenca/*difference*/ = nota1/*grade1*/ - nota2/*grade2*/;
     printf("O valor absoluto da diferenca de %d e %d eh: %d", nota1, nota2, abs(diferenca));//The absolute value of the difference of %d and %d is
 
 }
